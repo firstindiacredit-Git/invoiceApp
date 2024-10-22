148232fd7c536533dcabc71c5684e480c5687c59
PizeonFly <firsindiacredit786@gmail.com>
2024-10-22 11:31:40 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -125,9 +125,9 @@ export const forgotPassword = (req, res) => {
             subject: "Password reset request",
             html: `
                     <p>You requested for password reset from Pizeonfly Invoicing application</p>
-                    <h5>Please click this <a href=" http://localhost:3000/reset/${token}">link</a> to reset your password</h5>
+                    <h5>Please click this <a href="https://pizeonflyinvoiceapp.vercel.app/reset/${token}">link</a> to reset your password</h5>
                     <p>Link not clickable?, copy and paste the following url in your address bar.</p>
-                    <p>http://localhost:3000/reset/${token}</p>
+                    <p>https://pizeonflyinvoiceapp.vercel.app/reset/${token}</p>
                     <P>If this was a mistake, just ignore this email and nothing will happen.</P>
                     `,
           });
